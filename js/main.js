56520f7d3718127974c6212e8755afe770d3d32b
eduar <chanaga250@gmail.com>
2024-05-14 18:49:08 -0500

    feat: :sparkles: barras de progreso
    se agregaron las barras de progreso que muestran la informacion de los cohetes

--- js/main.js
+++ js/main.js
@@ -99,29 +99,191 @@ rocket.flickr_images.forEach(imageUrl => {
     });
 
 
-    // Mostrar motores
-    const enginesDiv = document.getElementById('information__2');
-    enginesDiv.innerHTML = `
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <h2>Motores</h2>
-        <p>ISP al Nivel del Mar: ${rocket.engines.isp.sea_level}</p>
-        <p>ISP en el Vacío: ${rocket.engines.isp.vacuum}</p>
-        <p>Empuje al Nivel del Mar: ${rocket.engines.thrust_sea_level.kN} kN (${rocket.engines.thrust_sea_level.lbf} lbf)</p>
-        <p>Empuje en el Vacío: ${rocket.engines.thrust_vacuum.kN} kN (${rocket.engines.thrust_vacuum.lbf} lbf)</p>
-        <p>Número de Motores: ${rocket.engines.number}</p>
-        <p>Tipo de Motor: ${rocket.engines.type}</p>
-        <p>Versión: ${rocket.engines.version}</p>
-        <p>Diseño: ${rocket.engines.layout}</p>
-        <p>Pérdida Máxima del Motor: ${rocket.engines.engine_loss_max}</p>
-        <p>Combustible 1: ${rocket.engines.propellant_1}</p>
-        <p>Combustible 2: ${rocket.engines.propellant_2}</p>
-        <p>Empuje a Peso: ${rocket.engines.thrust_to_weight}</p>
-    `;
+    // Mostrar motores como una barra de progreso
+const enginesDiv = document.getElementById('information__2');
+enginesDiv.innerHTML = `
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h2>Motores</h2>
+    <div class="progress-info">
+        <p>Tipo de Motor:</p>
+        <span>${rocket.engines.type}</span>
+    </div>
+    <div class="progress-info">
+        <p>Versión:</p>
+        <span>${rocket.engines.version}</span>
+    </div>
+    <div class="progress-info">
+        <p>Diseño:</p>
+        <span>${rocket.engines.layout}</span>
+    </div>
+    ${rocket.engines.engine_loss_max ? `
+        <div class="progress-info">
+            <p>Pérdida Máxima del Motor:</p>
+            <span>${rocket.engines.engine_loss_max}</span>
+        </div>
+    ` : ''}
+    ${rocket.engines.propellant_1 ? `
+        <div class="progress-info">
+            <p>Combustible 1:</p>
+            <span>${rocket.engines.propellant_1}</span>
+        </div>
+    ` : ''}
+    ${rocket.engines.propellant_2 ? `
+        <div class="progress-info">
+            <p>Combustible 2:</p>
+            <span>${rocket.engines.propellant_2}</span>
+        </div>
+    ` : ''}
+    ${rocket.engines.thrust_sea_level.kN ? `
+        <div class="progress-info">
+            <p>Empuje al Nivel del Mar:</p>
+            <span>${rocket.engines.thrust_sea_level.kN} kN (Max: 2000)</span>
+        </div>
+        <div class="progress">
+            <div class="progress-bar" style="width: ${(rocket.engines.thrust_sea_level.kN / 2000) * 100}%;"></div>
+        </div>
+    ` : ''}
+    ${rocket.engines.thrust_vacuum.kN ? `
+        <div class="progress-info">
+            <p>Empuje en el Vacío:</p>
+            <span>${rocket.engines.thrust_vacuum.kN} kN (Max: 2000)</span>
+        </div>
+        <div class="progress">
+            <div class="progress-bar" style="width: ${(rocket.engines.thrust_vacuum.kN / 2000) * 100}%;"></div>
+        </div>
+    ` : ''}
+    ${rocket.engines.thrust_to_weight ? `
+        <div class="progress-info">
+            <p>Empuje a Peso:</p>
+            <span>${rocket.engines.thrust_to_weight} (Max: 200)</span>
+        </div>
+        <div class="progress">
+            <div class="progress-bar" style="width: ${(rocket.engines.thrust_to_weight / 200) * 100}%;"></div>
+        </div>
+    ` : ''}
+`;
+// Crear el elemento SVG para la barra de progreso redonda de ISP en el Vacío
+const svgVacuum = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+svgVacuum.setAttribute('class', 'progress-circle');
+svgVacuum.setAttribute('width', '150'); // Ajustar el tamaño del SVG
+svgVacuum.setAttribute('height', '150'); // Ajustar el tamaño del SVG
+
+// Crear el círculo interno para la barra de progreso redonda de ISP en el Vacío
+const circleVacuum = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+circleVacuum.setAttribute('class', 'progress-circle-inner');
+circleVacuum.setAttribute('cx', '75'); // Posición x centrada
+circleVacuum.setAttribute('cy', '75'); // Posición y centrada
+circleVacuum.setAttribute('r', '65'); // Ajustar el radio del círculo
+circleVacuum.setAttribute('stroke-width', '20'); // Ajustar el ancho del trazo
+circleVacuum.setAttribute('fill', 'transparent');
+circleVacuum.setAttribute('stroke', '#14162C'); // Color de la barra de progreso
+
+// Calcular el porcentaje de progreso para ISP en el Vacío
+const progressPercentVacuum = (rocket.engines.isp.vacuum / 500) * 100;
+
+// Calcular la longitud de la barra de progreso redonda para ISP en el Vacío
+const circumferenceVacuum = 2 * Math.PI * parseFloat(circleVacuum.getAttribute('r'));
+const strokeDasharrayVacuum = `${(progressPercentVacuum * circumferenceVacuum) / 100} ${circumferenceVacuum}`;
+
+// Establecer la longitud de la barra de progreso redonda para ISP en el Vacío
+circleVacuum.style.strokeDasharray = strokeDasharrayVacuum;
+
+// Agregar el círculo al SVG de ISP en el Vacío
+svgVacuum.appendChild(circleVacuum);
+
+// Crear el texto para mostrar el nombre y el valor de ISP en el Vacío
+const textVacuum = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+textVacuum.setAttribute('x', '50%');
+textVacuum.setAttribute('y', '50%'); // Posición y del texto
+textVacuum.setAttribute('text-anchor', 'middle');
+textVacuum.setAttribute('dominant-baseline', 'middle');
+textVacuum.setAttribute('font-size', '0.7vi'); // Tamaño del texto
+textVacuum.setAttribute('fill', '#fff'); // Color del texto
+textVacuum.textContent = `ISP-nivel del Vacío: ${rocket.engines.isp.vacuum}`;
+
+// Agregar el texto al SVG de ISP en el Vacío
+svgVacuum.appendChild(textVacuum);
+
+// Obtener el div existente para ISP en el Vacío dentro de .section__information__3
+const vacuumDiv = document.querySelector('.section__information__3 > div');
+
+// Agregar el SVG de ISP en el Vacío al div existente dentro de .section__information__3
+if (vacuumDiv) {
+    vacuumDiv.innerHTML = ''; // Limpiar el contenido existente
+    vacuumDiv.appendChild(svgVacuum);
+} else {
+    console.error('No se encontró el div para ISP en el Vacío dentro de .section__information__3');
+}
+
+
+
+// Crear el elemento SVG para la barra de progreso redonda
+const svgNS = 'http://www.w3.org/2000/svg';
+const svg = document.createElementNS(svgNS, 'svg');
+svg.setAttribute('class', 'progress-circle');
+svg.setAttribute('width', '150'); // Aumentar el tamaño del SVG
+svg.setAttribute('height', '150'); // Aumentar el tamaño del SVG
+
+// Crear el círculo interno para la barra de progreso redonda
+const circle = document.createElementNS(svgNS, 'circle');
+circle.setAttribute('class', 'progress-circle-inner');
+circle.setAttribute('cx', '75'); // Posición x centrada
+circle.setAttribute('cy', '75'); // Posición y centrada
+circle.setAttribute('r', '65'); // Aumentar el radio del círculo
+circle.setAttribute('stroke-width', '20'); // Aumentar el ancho del trazo
+circle.setAttribute('fill', 'transparent');
+circle.setAttribute('stroke', '#14162C'); // Color de la barra de progreso
+circle.style.transition = 'stroke-dasharray 0.3s ease';
+
+// Agregar el círculo al SVG
+svg.appendChild(circle);
+
+// Calcular el porcentaje de progreso
+const progressPercent = (rocket.engines.isp.sea_level / 400) * 100;
+
+// Calcular la longitud de la barra de progreso redonda
+const circumference = 2 * Math.PI * parseFloat(circle.getAttribute('r'));
+const strokeDasharray = `${(progressPercent * circumference) / 100} ${circumference}`;
+
+// Establecer la longitud de la barra de progreso redonda
+circle.style.strokeDasharray = strokeDasharray;
+
+// Crear el elemento texto dentro del SVG para el nombre y valor
+const text = document.createElementNS(svgNS, 'text');
+text.setAttribute('x', '50%');
+text.setAttribute('y', '50%'); // Ajustar la posición y del texto
+text.setAttribute('text-anchor', 'middle');
+text.setAttribute('dominant-baseline', 'middle');
+text.setAttribute('font-size', '0.75vi'); // Aumentar el tamaño del texto
+text.setAttribute('fill', '#fff'); // Color del texto
+text.textContent = `ISP-Nivel del Mar: ${rocket.engines.isp.sea_level}`;
+
+// Agregar el texto al SVG
+svg.appendChild(text);
+
+// Obtener el div existente para ISP al Nivel del Mar
+const ispSeaLevelDiv = document.querySelector('.section__information__2');
+
+// Agregar el SVG al div existente dentro de .section__information__2
+if (ispSeaLevelDiv) {
+    const innerDiv = ispSeaLevelDiv.querySelector('div');
+    if (innerDiv) {
+        innerDiv.innerHTML = ''; // Limpiar el contenido existente
+        innerDiv.appendChild(svg);
+    } else {
+        console.error('No se encontró el div interno dentro de .section__information__2');
+    }
+} else {
+    console.error('No se encontró el div para ISP al Nivel del Mar dentro de .section__information__2');
+}
+
+
+
 
     //creacion de botones para paginacion
     const pag = document.getElementById('paginacion');
@@ -207,6 +369,7 @@ function capsule(){
                 let logo = document.getElementById('section__information__1');
                 logo.innerHTML = '<h2>Esta cápsula no tiene lanzamientos.</h2>';
             }
+            
             let capsuleInformation = document.getElementById('information__2');
             capsuleInformation.innerHTML = `
             <br>
